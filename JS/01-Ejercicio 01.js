4eebd397a4d161dd30f48be4f20460032dca7fc1
Alejo3D <alejandro_insinga@hotmail.com>
2025-04-27 14:20:22 -0300

    Ejercicio 1 terminado

---
+++ JS/01-Ejercicio 01.js
@@ -0,0 +1,9 @@
+let meses = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+  
+  for (let i = 0; i < meses.length; i++) {
+    document.writeln((i + 1) + ". " + meses[i] + "<br>");
+  }
+  
