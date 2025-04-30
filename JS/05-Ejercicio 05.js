a55fd8da6823c6ed9cffc4d2d43a6d1a40656cdc
Alejo3D <alejandro_insinga@hotmail.com>
2025-04-30 17:57:07 -0300

    Ejercicio 5 terminado

---
+++ JS/05-Ejercicio 05.js
@@ -0,0 +1,13 @@
+function analizarCadena(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      return "La cadena está formada solo por mayúsculas";
+    } else if (cadena === cadena.toLowerCase()) {
+      return "La cadena está formada solo por minúsculas";
+    } else {
+      return "La cadena está formada por una mezcla de mayúsculas y minúsculas";
+    }
+  }
+  
+  let texto = prompt("Ingresa una cadena de texto:");
+  document.writeln(analizarCadena(texto));
+  
